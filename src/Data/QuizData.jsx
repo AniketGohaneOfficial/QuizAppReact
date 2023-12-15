7f1e202cf991891de4b5393bf86c2e9f77795cfc
AniketGohaneOfficial <aniketgohaneofficial@gmail.com>
2023-12-16 01:04:35 +0530

    First Message

---
+++ src/Data/QuizData.jsx
@@ -0,0 +1,27 @@
+export const QuizData = [
+    {
+        question: "National Animal  ?",
+        options: [ "Tiger", "Lion", "Elephant", "Horse" ],
+        answer: 1,
+    },
+    {
+        question: "Capital of India ?",
+        options: [ "Mumbai", "New-Delhi", "Bangalore", "Hyderabad" ],
+        answer: 2,
+    },
+    {
+        question: "Capital of Rajasthan ?",
+        options: [ "Jaipur", "Udaipur", "Ajmer", "Bhopal" ],
+        answer: 1,
+    },
+    {
+        question: "Capital of Maharashatra ?",   
+        options: [ "Pue", "Nashik", "Aurangabad", "Mumbai" ],   
+        answer: 4,
+    },
+    {
+        question: "National Bird of India ?",
+        options: [ "Hen", "Eagle", "Peacock", "Ostrice" ],
+        answer: 3,
+    }
+];
